2c17487eb3514de666e11f4e98f8572672cc76b5
Aditya Pratap Singh <thisisaditya.me@gmail.com>
2025-02-06 16:19:58 +0530

    import, read and show terrain map

--- main.cpp
+++ main.cpp
@@ -1,104 +1,102 @@
-#include <SFML/Graphics.hpp>
-#include <random>
+#include <sfml/Graphics.hpp>
 #include <iostream>
-
-using namespace std;
+#include <vector>
 
 int main() {
+    // Load the terrain map
+    sf::Image terrain_map;
+    if (terrain_map.loadFromFile("terrain_map.png")) {
+        std::cerr << "terrain map loaded" << std::endl;
+    }
+    else {
+        std::cerr << "failed to load terrain_map.png" << std::endl;
+    }
 
-    // SFML window settings
-    sf::ContextSettings settings;
-    settings.antialiasingLevel = 8;
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "Main menu", sf::Style::Default, settings);
-    window.setFramerateLimit(144); // set frame rate limit
-    window.setVerticalSyncEnabled(true); // enable vertical sync
+    // Get the terrain map size
+    sf::Vector2u terrain_map_size = terrain_map.getSize();
 
-    // initialize data
-    int squareSize = 18;
-    int padding = 1;
-    int tileSize = squareSize + padding * 2;
+    // make the 3d terrain grid
+    static const int grid_size_x = terrain_map_size.x;
+    static const int grid_size_y = terrain_map_size.y;
+    static const int grid_size_z = 4; // 0 = food type, 1 = water type, 2 = terrain type , 3 = food level
+    int terrain[grid_size_x][grid_size_y][grid_size_z] = { 0 };
 
-    int xTiles = window.getSize().x / tileSize;
-    int yTiles = window.getSize().y / tileSize;
-
-    // Create the map with properties.
-    int map[128][72][10];
-    // x and y axis the first 2 and third one contains details about each tile.
-
-    // set each tile on the map randomly to either water or land.
-    for (int i = 0;i < xTiles;i++) {
-        for (int j = 0;j < yTiles;j++) {
-            map[i][j][0] = rand() % 16; // water or land
-            map[i][j][1] = rand() % 3; // food
+    // insert data in the terrain array
+    for (unsigned int y = 0; y < terrain_map_size.y;y++) {
+        for (unsigned int x = 0; x < terrain_map_size.x;x++) {
+            sf::Color pixel = terrain_map.getPixel(x, y);
+            if (pixel.r == 168 && pixel.g == 230 && pixel.b == 29) {
+                terrain[x][y][2] = 0; // normal terrain
+            }
+            else if (pixel.r == 255 && pixel.g == 242 && pixel.b == 0) {
+                terrain[x][y][2] = 1; // desert terrain
+            }
+            else if (pixel.r == 34 && pixel.g == 177 && pixel.b == 76) {
+                terrain[x][y][2] = 2; // forest terrain
+            }
+            else if (pixel.r == 0 && pixel.g == 183 && pixel.b == 239) {
+                terrain[x][y][2] = 3; // snow terrain
+            }
+            else if (pixel.r == 156 && pixel.g == 90 && pixel.b == 60) {
+                terrain[x][y][2] = 4; // water terrain
+            }
         }
     }
 
-    // frames counter
-    int nFrames = 0;
+    // make the main array of creatures and their properties
+    // x = creature number, 0 = x_cordinates, 2 = y_cordinates, 3 = food, 4 = water, 5 = health, 6 = age
+    std::vector<std::vector<int>> creatures;
 
-    // water color reducer
-    int waterGradientConstant = 0; // upto 15
+    int tile_size = 40;
 
+    int window_size_x = grid_size_x * tile_size;
+    int window_size_y = grid_size_y * tile_size;
+
+    // display window setup
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode(window_size_x, window_size_y), "Creature Sim");
+    window.setFramerateLimit(144); // set frame rate limit
+    window.setVerticalSyncEnabled(true); // enable vertical sync
+
+
+    // main loop
     while (window.isOpen()) {
-
-        // frame counter
-        nFrames++;
-
-        // update water color every 144 frames
-        if (nFrames == 144 * 1) {
-            waterGradientConstant++;
-        }
-
-        // Event handling
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
-        }
-
-        // Draw the map
-        window.clear(sf::Color::Black);
-
-        for (int i = 0; i < xTiles; i++) {
-            for (int j = 0; j < yTiles; j++) {
-                // Draw the tile
-                sf::RectangleShape tile(sf::Vector2f(squareSize, squareSize));
-                tile.setPosition(i * tileSize + padding, j * tileSize + padding);
-                // // water can dry and turn into land
-                if (nFrames == 144 * 1 && map[i][j][0] < 15) {
-                    map[i][j][0]++;
-                }
-                // if (map[x][y][0] == 0 && 1) {
-                tile.setFillColor(sf::Color(map[i][j][0] * 17, 121 + map[i][j][0] * 4, 255 - (map[i][j][0] * 16)));
-                // }
-                // else {
-                //     tile.setFillColor(sf::Color(255, 181, 15));
-                // }
-
-                window.draw(tile);
-            }
-        }
-
-        for (int i = 0; i < xTiles; i++) {
-            for (int j = 0;j < yTiles;j++) {
-                // Draw the food
-                if (map[i][j][1] == 1) {
-                    sf::CircleShape food(3);
-                    food.setFillColor(sf::Color::Green);
-                    food.setPosition(i * tileSize + padding + 6, j * tileSize + padding + 6);
-                    window.draw(food);
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Escape) {
+                    window.close();
                 }
             }
         }
-
+        window.clear();
+        // displaying the terrain map
+        for (unsigned int y = 0; y < terrain_map_size.y;y++) {
+            for (unsigned int x = 0; x < terrain_map_size.x;x++) {
+                sf::RectangleShape rectangle(sf::Vector2f(tile_size - 2, tile_size - 2));
+                rectangle.setPosition(x * tile_size + 1, y * tile_size + 1);
+                if (terrain[x][y][2] == 0) {
+                    rectangle.setFillColor(sf::Color(168, 230, 29));
+                }
+                else if (terrain[x][y][2] == 1) {
+                    rectangle.setFillColor(sf::Color(255, 242, 0));
+                }
+                else if (terrain[x][y][2] == 2) {
+                    rectangle.setFillColor(sf::Color(34, 177, 76));
+                }
+                else if (terrain[x][y][2] == 3) {
+                    rectangle.setFillColor(sf::Color(0, 183, 239));
+                }
+                else if (terrain[x][y][2] == 4) {
+                    rectangle.setFillColor(sf::Color(156, 90, 60));
+                }
+                window.draw(rectangle);
+            }
+        }
         window.display();
-
-        // reset frame counter
-        if (nFrames == 144) {
-            nFrames = 0;
-        }
     }
-
-    return 0;
 }
