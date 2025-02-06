bdf5be7107351a68b4f27f7137eb86dddf6c6165
Aditya Pratap Singh <thisisaditya.me@gmail.com>
2025-02-06 19:20:05 +0530

    initial water generation and display

--- main.cpp
+++ main.cpp
@@ -81,10 +81,77 @@ int assign_food(int terrain_type) {
             return 6; // Nothing
         }
     }
+    else {
+        std::cout << "error in assign_food" << std::endl;
+    }
     return 6;
 }
 
+int assign_water(int terrain_type) {
+    int roullete = rand() % 100;
+    if (terrain_type == 0) { // plains
+        if (roullete < 10) {
+            return 0; // water logged
+        }
+        else if (roullete < 85) {
+            return 1; // humid
+        }
+        else {
+            return 2; // dry
+        }
+    }
+    else if (terrain_type == 1) {
+        if (roullete < 5) {
+            return 0; // water logged
+        }
+        else if (roullete < 15) {
+            return 1; // humid
+        }
+        else {
+            return 2; // dry
+        }
+    }
+    else if (terrain_type == 2) {
+        if (roullete < 10) {
+            return 0; // water logged
+        }
+        else if (roullete < 95) {
+            return 1; // humid
+        }
+        else {
+            return 2; // dry
+        }
+    }
+    else if (terrain_type == 3) {
+        if (roullete < 30) {
+            return 0; // water logged
+        }
+        else if (roullete < 40) {
+            return 1; // humid
+        }
+        else {
+            return 3; // ice
+        }
+    }
+    else if (terrain_type == 4) {
+        if (roullete < 5) {
+            return 0; // water logged
+        }
+        else if (roullete < 95) {
+            return 1; // humid
+        }
+        else {
+            return 3; // ice
+        }
+    }
+    else {
+        std::cout << "error in assign_water" << std::endl;
+    }
+    return 1;
+}
+
 int main() {
+    srand(time(0));
     // Load the terrain map
     sf::Image terrain_map;
     if (terrain_map.loadFromFile("terrain_map.png")) {
@@ -108,24 +175,32 @@ int main() {
         for (unsigned int x = 0; x < terrain_map_size.x;x++) {
             sf::Color pixel = terrain_map.getPixel(x, y);
             if (pixel.r == 168 && pixel.g == 230 && pixel.b == 29) {
-                terrain[x][y][2] = 0; // normal terrain
                 terrain[x][y][0] = assign_food(0);
+                terrain[x][y][1] = assign_water(0);
+                terrain[x][y][2] = 0; // normal terrain
             }
             else if (pixel.r == 255 && pixel.g == 242 && pixel.b == 0) {
-                terrain[x][y][2] = 1; // desert terrain
                 terrain[x][y][0] = assign_food(1);
+                terrain[x][y][1] = assign_water(1);
+                terrain[x][y][2] = 1; // desert terrain
             }
             else if (pixel.r == 34 && pixel.g == 177 && pixel.b == 76) {
-                terrain[x][y][2] = 2; // forest terrain
                 terrain[x][y][0] = assign_food(2);
+                terrain[x][y][1] = assign_water(2);
+                terrain[x][y][2] = 2; // forest terrain
             }
             else if (pixel.r == 0 && pixel.g == 183 && pixel.b == 239) {
-                terrain[x][y][2] = 3; // snow terrain
                 terrain[x][y][0] = assign_food(3);
+                terrain[x][y][1] = assign_water(3);
+                terrain[x][y][2] = 3; // snow terrain
             }
             else if (pixel.r == 156 && pixel.g == 90 && pixel.b == 60) {
-                terrain[x][y][2] = 4; // water terrain
                 terrain[x][y][0] = assign_food(4);
+                terrain[x][y][1] = assign_water(4);
+                terrain[x][y][2] = 4; // water terrain
+            }
+            else {
+                std::cout << "miss-colored pixel at: " << x << ", " << y << std::endl;
             }
         }
     }
@@ -146,9 +221,12 @@ int main() {
     window.setFramerateLimit(144); // set frame rate limit
     window.setVerticalSyncEnabled(true); // enable vertical sync
 
-
+    sf::sleep(sf::milliseconds(100));
+    unsigned long long int clock = 0;
     // main loop
     while (window.isOpen()) {
+        // sf::sleep(sf::milliseconds(10));
+        // clock++;
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
@@ -216,6 +294,30 @@ int main() {
                 }
                 window.draw(rectangle);
                 window.draw(food);
+            }
+        }
+        // displaying the foods
+        for (unsigned int y = 0; y < terrain_map_size.y;y++) {
+            for (unsigned int x = 0; x < terrain_map_size.x;x++) {
+                sf::RectangleShape rectangle(sf::Vector2f(tile_size / 5 - 1, tile_size - 2)); // outline of food
+                rectangle.setPosition(x * tile_size + (tile_size - tile_size / 5), y * tile_size + 1);
+                sf::RectangleShape water(sf::Vector2f(tile_size / 5 - 3, tile_size - 4)); // food
+                water.setPosition(x * tile_size + (tile_size - tile_size / 5) + 1, y * tile_size + 2);
+                rectangle.setFillColor(sf::Color(255, 255, 255));
+                if (terrain[x][y][1] == 0) {
+                    water.setFillColor(sf::Color(0, 112, 255));
+                }
+                else if (terrain[x][y][1] == 1) {
+                    water.setFillColor(sf::Color(0, 215, 139));
+                }
+                else if (terrain[x][y][1] == 2) {
+                    water.setFillColor(sf::Color(255, 255, 102));
+                }
+                else {
+                    water.setFillColor(sf::Color(103, 248, 252));
+                }
+                window.draw(rectangle);
+                window.draw(water);
             }
         }
         window.display();
